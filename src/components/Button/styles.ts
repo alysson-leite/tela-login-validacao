330a5437eb0a15f5f962d60b298f9d5b44d5694f
Alysson Leite <alysson_leite@yahoo.com.br>
2022-12-21 10:10:55 -0300

    first commit

---
+++ src/components/Button/styles.ts
@@ -0,0 +1,23 @@
+import styled from 'styled-components';
+
+
+export const ButtonContainer = styled.button`
+    width: 100%;
+    height: 42px;
+    background-color: #81259D;
+    color: #FFF;
+
+    border: 1px solid #81259D;
+    border-radius: 21px;
+
+    transition: .3s;
+
+    &:hover {
+        opacity: 0.8;
+        cursor:pointer;
+    }
+
+    &:active {
+        opacity: 0.6;
+    }
+`
